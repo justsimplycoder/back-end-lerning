10755e3407aa03f128547221932afbf43592745c
justsimplycoder <justsimplycoder@gmail.com>
2023-08-23 20:23:06 +0400

    Добавление: транслитиризация node

---
+++ be-node/translit-node/index.js
@@ -0,0 +1,80 @@
+const CyrillicToTranslit = require('cyrillic-to-translit-js');
+
+/*
+	Транслит npm пакет cyrillic-to-translit-js
+ */
+const cyrillicToTranslit = new CyrillicToTranslit();
+console.log(
+	cyrillicToTranslit.transform('Всем привет!')
+);
+// -> Vsem privet!
+console.log(
+	cyrillicToTranslit.transform('Всем привет!', '-').toLowerCase()
+);
+// -> vsem-privet!
+
+
+function translit(str){
+	let answer = '';
+	const converter = {
+		'а': 'a',    'б': 'b',    'в': 'v',    'г': 'g',    'д': 'd',
+		'е': 'e',    'ё': 'e',    'ж': 'zh',   'з': 'z',    'и': 'i',
+		'й': 'y',    'к': 'k',    'л': 'l',    'м': 'm',    'н': 'n',
+		'о': 'o',    'п': 'p',    'р': 'r',    'с': 's',    'т': 't',
+		'у': 'u',    'ф': 'f',    'х': 'h',    'ц': 'c',    'ч': 'ch',
+		'ш': 'sh',   'щ': 'sch',  'ь': '',     'ы': 'y',    'ъ': '',
+		'э': 'e',    'ю': 'yu',   'я': 'ya',
+
+		'А': 'A',    'Б': 'B',    'В': 'V',    'Г': 'G',    'Д': 'D',
+		'Е': 'E',    'Ё': 'E',    'Ж': 'Zh',   'З': 'Z',    'И': 'I',
+		'Й': 'Y',    'К': 'K',    'Л': 'L',    'М': 'M',    'Н': 'N',
+		'О': 'O',    'П': 'P',    'Р': 'R',    'С': 'S',    'Т': 'T',
+		'У': 'U',    'Ф': 'F',    'Х': 'H',    'Ц': 'C',    'Ч': 'Ch',
+		'Ш': 'Sh',   'Щ': 'Sch',  'Ь': '',     'Ы': 'Y',    'Ъ': '',
+		'Э': 'E',    'Ю': 'Yu',   'Я': 'Ya'
+	};
+
+	for (let i = 0; i < str.length; ++i ) {
+		if (converter[str[i]] == undefined){
+			answer += str[i];
+		} else {
+			answer += converter[str[i]];
+		}
+	}
+
+	return answer;
+}
+
+console.log(translit('Всем привет!'));
+// -> Vsem privet!
+
+function translitPath(str){
+	const converter = {
+		'а': 'a',    'б': 'b',    'в': 'v',    'г': 'g',    'д': 'd',
+		'е': 'e',    'ё': 'e',    'ж': 'zh',   'з': 'z',    'и': 'i',
+		'й': 'y',    'к': 'k',    'л': 'l',    'м': 'm',    'н': 'n',
+		'о': 'o',    'п': 'p',    'р': 'r',    'с': 's',    'т': 't',
+		'у': 'u',    'ф': 'f',    'х': 'h',    'ц': 'c',    'ч': 'ch',
+		'ш': 'sh',   'щ': 'sch',  'ь': '',     'ы': 'y',    'ъ': '',
+		'э': 'e',    'ю': 'yu',   'я': 'ya'
+	};
+
+	str = str.toLowerCase();
+
+	let answer = '';
+	for (let i = 0; i < str.length; ++i ) {
+		if (converter[str[i]] == undefined){
+			answer += str[i];
+		} else {
+			answer += converter[str[i]];
+		}
+	}
+
+	answer = answer.replace(/[^-0-9a-z]/g, '-');
+	answer = answer.replace(/[-]+/g, '-');
+	answer = answer.replace(/^\-|-$/g, '');
+	return answer;
+}
+
+console.log(translitPath('Всем привет!'));
+// -> vsem-privet
