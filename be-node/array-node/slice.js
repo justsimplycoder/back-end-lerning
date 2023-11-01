d7349908da2c7a1ddb8172a6162204e7f6c9605d
justsimplycoder <justsimplycoder@gmail.com>
2023-11-01 20:46:28 +0400

    Добавление: array->slice js

---
+++ be-node/array-node/slice.js
@@ -0,0 +1,12 @@
+console.log(
+    ['a', 'b', 'c', 'd', 'e'].slice(2)
+);
+// -> [ 'c', 'd', 'e' ]
+console.log(
+    ['a', 'b', 'c', 'd', 'e'].slice(1, 3)
+);
+// -> [ 'b', 'c' ]
+console.log(
+    ['a', 'b', 'c', 'd', 'e'].slice(-2, -1)
+);
+// -> [ 'd' ]
