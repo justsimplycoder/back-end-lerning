5ad65357d4b3df075d09ef1577dac54fed1e5ce6
justsimplycoder <justsimplycoder@gmail.com>
2023-11-14 20:38:20 +0400

    Добавление: array->shift node

---
+++ be-node/array-node/shift.js
@@ -0,0 +1,8 @@
+const fruits = ["orange", "banana", "apple", "raspberry"];
+
+let fruit = fruits.shift();
+
+console.log(fruit);
+// -> orange
+console.log(fruits);
+// -> [ 'banana', 'apple', 'raspberry' ]
