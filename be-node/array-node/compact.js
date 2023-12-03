c5da38f39965fda46f14e3cf1281ecdecf6f048a
justsimplycoder <justsimplycoder@gmail.com>
2023-12-03 12:03:14 +0400

    Добавление: array->compact node

---
+++ be-node/array-node/compact.js
@@ -0,0 +1,11 @@
+const city  = "San Francisco";
+const state = "CA";
+const event = "SIGGRAPH";
+
+const result = {
+    city,
+    state,
+    event
+};
+console.log(result);
+// -> { city: 'San Francisco', state: 'CA', event: 'SIGGRAPH' }
