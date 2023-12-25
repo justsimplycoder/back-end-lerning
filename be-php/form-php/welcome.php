6a0deae6d1c91fc4aefa839dd0ef12af5c9928a0
justsimplycoder <justsimplycoder@gmail.com>
2023-12-25 22:34:12 +0400

    Добавление: form->simple php

---
+++ be-php/form-php/welcome.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Страница преветствия</title>
+</head>
+<body>
+	<h1>Привет <?php echo $_POST['lastname'] . ' ' . $_POST['firstname'] . '!'; ?></h1>
+</body>
+</html>
