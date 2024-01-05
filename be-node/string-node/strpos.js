8d6a14a7944cdce13bb284b1f509e05e92cea305
justsimplycoder <justsimplycoder@gmail.com>
2024-01-05 20:01:41 +0400

    Изменение: string->strpos

--- be-node/string-node/strpos.js
+++ be-node/string-node/strpos.js
@@ -1,1 +1,9 @@
-console.log('text lorem ipsum'.indexOf('lorem')); // -> 5
+const str = 'Hello world!';
+console.log(str.indexOf('world')); // -> 6
+
+if (str.indexOf('Bye') != -1) {
+	console.log('true');
+} else {
+	console.log('false');
+}
+// -> false
--- be-php/string-php/strpos.php
+++ be-php/string-php/strpos.php
@@ -1,3 +1,15 @@
 <?php
-echo mb_strpos('text lorem ipsum', 'lorem'); // -> 5
- ?>
+$str_en = "Hello world!";
+echo strpos($str_en, "world"); // -> 6
+echo PHP_EOL;
+$str_ru = "Привет, мир!";
+echo mb_strpos($str_ru, "мир"); // -> 8
+echo PHP_EOL;
+
+// Проверка вхождения строки в подстроку только с оператором эквивалентность (строгое сравнение)
+if (strpos($str_en, "Bye") !== false) {
+	echo "true";
+} else {
+	echo "false";
+}
+// -> false
