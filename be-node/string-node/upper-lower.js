9182a22461210600108c7f0bd082ad1d12254420
justsimplycoder <justsimplycoder@gmail.com>
2024-01-05 13:46:30 +0400

    Добавление: string->upper and lower node

---
+++ be-node/string-node/upper-lower.js
@@ -0,0 +1,7 @@
+let str_en = "Text";
+let str_ru = "Текст";
+
+console.log(str_en.toUpperCase()); // -> TEXT
+console.log(str_ru.toUpperCase()); // -> ТЕКСТ
+console.log(str_en.toLowerCase()); // -> text
+console.log(str_ru.toLowerCase()); // -> текст
